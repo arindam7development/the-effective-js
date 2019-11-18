42ee069577ba61db6b0122c3eeea63acb2cde9b4
arindam <arindam77577@gmail.com>
2019-11-18 21:19:42 +0530

    functionexperiment.js

--- functionexperiment.js
+++ functionexperiment.js
@@ -14,13 +14,18 @@ arindam(function(){
 //If i am trying to send a function
 
 arindam(function() {
-    console.log("arindam")
+    return "arindam"
+})
+
+arindam(function() {
+    return 500
 })
 
 function arindam( a) {
 
     if (typeof a === "function") {
-        a()
+        result = a()
+        console.log(result)
     }else {
         console.log(a)
     }
